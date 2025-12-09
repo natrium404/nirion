71929c3b5d878ea0f027b4ddac316ab2a91615fe
Natrium <hanako.yugiamane101@gmail.com>
2025-12-09 15:03:20 +0600

    chore: remove deprecated scripts and package files; update pkglist.sh

--- installer/modules/pkglist.sh
+++ installer/modules/pkglist.sh
@@ -1,32 +1,32 @@
 #!/usr/bin/env bash
 
 pkglist_init() {
-	log STEP "Initializing pkglist module"
+  log STEP "Initializing pkglist module"
 }
 
 pkglist_validate() {
-	[ "${ENABLE_PKGLIST}" = "true" ]
+  [ "${ENABLE_PKGLIST}" = "true" ]
 }
 
 pkglist_run() {
-	local PKG_DIR="$HOME/.nirion/packages"
+  local PKG_DIR="$HOME/.nirion/scripts/packages"
 
-	if [ -d "${PKG_DIR}" ]; then
-		if [ -f "${PKG_DIR}/pkg" ]; then
-			mkdir -p "$HOME/bin"
-			create_symlink "${PKG_DIR}/pkg" "$HOME/bin/pkg"
+  if [ -d "${PKG_DIR}" ]; then
+    if [ -f "${PKG_DIR}/pkg" ]; then
+      mkdir -p "$HOME/bin"
+      create_symlink "${PKG_DIR}/pkg" "$HOME/bin/pkg"
 
-			SUMMARY_STATUS[pkglist]="Done"
-		else
-			log WARN "No pkg script in $PKG_DIR folder"
-			SUMMARY_STATUS[pkglist]="Skipped"
-		fi
-	else
-		log WARN "No package folder in setup folder"
-		SUMMARY_STATUS[pkglist]="Skipped"
-	fi
+      SUMMARY_STATUS[pkglist]="Done"
+    else
+      log WARN "No pkg script in $PKG_DIR folder"
+      SUMMARY_STATUS[pkglist]="Skipped"
+    fi
+  else
+    log WARN "No package folder in setup folder"
+    SUMMARY_STATUS[pkglist]="Skipped"
+  fi
 }
 
 pkglist_summary() {
-	log INFO "pkglist module status: ${SUMMARY_STATUS[pkglist]}"
+  log INFO "pkglist module status: ${SUMMARY_STATUS[pkglist]}"
 }
